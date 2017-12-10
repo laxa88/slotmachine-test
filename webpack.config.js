ab17a2eefef69c412aecaeb66c2c028af7243a90
laxa88 <gashzack@hotmail.com>
2017-12-11 01:45:02 +0800

    Add sourcemap to webpack.config for debugging

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
+  devtool: 'source-map',
   entry: './app.js',
   output: {
     filename: './build/bundle.js',
   },
   plugins: [
-    new UglifyJSPlugin(),
+    new UglifyJSPlugin({
+      sourceMap: true,
+    }),
   ],
 };
