075f4b500937bce5068653d5b0c5b1069cbc6921
laxa88 <gashzack@hotmail.com>
2017-12-11 01:22:31 +0800

    Update ICON_DATA to use constant values

--- src/constants.js
+++ src/constants.js
@@ -2,10 +2,16 @@ export const REEL_IDLE = 'REEL_IDLE';
 export const REEL_SPINNING = 'REEL_SPINNING';
 export const REEL_STOPPING = 'REEL_STOPPING';
 
+export const ICON_APPLE = 'apple';
+export const ICON_ORANGE = 'orange';
+export const ICON_BANANA = 'banana';
+export const ICON_GRAPE = 'grape';
+export const ICON_CHERRY = 'cherry';
+
 export const ICON_DATA = [
-  {key: 'apple', value: 1},
-  {key: 'orange', value: 2},
-  {key: 'banana', value: 3},
-  {key: 'grape', value: 5},
-  {key: 'cherry', value: 8},
+  {key: ICON_APPLE, value: 1},
+  {key: ICON_ORANGE, value: 2},
+  {key: ICON_BANANA, value: 3},
+  {key: ICON_GRAPE, value: 5},
+  {key: ICON_CHERRY, value: 8},
 ];
