a6171fc9ce996a55e33664ab6f53361cc88f2ae7
laxa88 <gashzack@hotmail.com>
2017-12-11 01:28:18 +0800

    Update constants, add sprite keys

--- src/constants.js
+++ src/constants.js
@@ -2,16 +2,19 @@ export const REEL_IDLE = 'REEL_IDLE';
 export const REEL_SPINNING = 'REEL_SPINNING';
 export const REEL_STOPPING = 'REEL_STOPPING';
 
-export const ICON_APPLE = 'apple';
-export const ICON_ORANGE = 'orange';
-export const ICON_BANANA = 'banana';
-export const ICON_GRAPE = 'grape';
-export const ICON_CHERRY = 'cherry';
+export const SPR_FOREGROUND = 'slotmachine-foreground';
+export const SPR_BUTTON = 'button';
+
+export const SPR_APPLE = 'apple';
+export const SPR_ORANGE = 'orange';
+export const SPR_BANANA = 'banana';
+export const SPR_GRAPE = 'grape';
+export const SPR_CHERRY = 'cherry';
 
 export const ICON_DATA = [
-  {key: ICON_APPLE, value: 1},
-  {key: ICON_ORANGE, value: 2},
-  {key: ICON_BANANA, value: 3},
-  {key: ICON_GRAPE, value: 5},
-  {key: ICON_CHERRY, value: 8},
+  {key: SPR_APPLE, value: 1},
+  {key: SPR_ORANGE, value: 2},
+  {key: SPR_BANANA, value: 3},
+  {key: SPR_GRAPE, value: 5},
+  {key: SPR_CHERRY, value: 8},
 ];
