87610e078455809fcc2ef60fac6dfda0e8e5f573
laxa88 <gashzack@hotmail.com>
2017-12-12 21:42:53 +0800

    Update constants.ICON_DATA

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,8 @@
+export const ICON_SIZE = 64;
+
+export const MACHINE_IDLE = 'MACHINE_IDLE';
+export const MACHINE_SPINNING = 'MACHINE_SPINNING';
+
 export const REEL_IDLE = 'REEL_IDLE';
 export const REEL_SPINNING = 'REEL_SPINNING';
 export const REEL_STOPPING = 'REEL_STOPPING';
@@ -5,16 +10,19 @@ export const REEL_STOPPING = 'REEL_STOPPING';
 export const SPR_FOREGROUND = 'slotmachine-foreground';
 export const SPR_BUTTON = 'button';
 
-export const SPR_APPLE = 'apple';
-export const SPR_ORANGE = 'orange';
-export const SPR_BANANA = 'banana';
-export const SPR_GRAPE = 'grape';
-export const SPR_CHERRY = 'cherry';
+export const SPR_SHEET = 'icons';
 
 export const ICON_DATA = [
-  {key: SPR_APPLE, value: 1},
-  {key: SPR_ORANGE, value: 2},
-  {key: SPR_BANANA, value: 3},
-  {key: SPR_GRAPE, value: 5},
-  {key: SPR_CHERRY, value: 8},
+  {name: 'behance.png', x: 32, y: 32, width: 32, height: 32, value: 1},
+  {name: 'dribbble.png', x: 64, y: 64, width: 32, height: 32, value: 1},
+  {name: 'facebook.png', x: 64, y: 32, width: 32, height: 32, value: 1},
+  {name: 'google.png', x: 64, y: 0, width: 32, height: 32, value: 1},
+  {name: 'like.png', x: 32, y: 96, width: 32, height: 32, value: 1},
+  {name: 'linkedin.png', x: 32, y: 64, width: 32, height: 32, value: 1},
+  {name: 'pinterest.png', x: 64, y: 96, width: 32, height: 32, value: 1},
+  {name: 'rss.png', x: 32, y: 0, width: 32, height: 32, value: 1},
+  {name: 'stumbleupon.png', x: 0, y: 96, width: 32, height: 32, value: 1},
+  {name: 'twitter.png', x: 0, y: 64, width: 32, height: 32, value: 1},
+  {name: 'vimeo.png', x: 0, y: 32, width: 32, height: 32, value: 1},
+  {name: 'youtube.png', x: 0, y: 0, width: 32, height: 32, value: 1},
 ];
