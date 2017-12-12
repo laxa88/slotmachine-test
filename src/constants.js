2c2d90a12b8fe95fc808c8a26ba989cd3bdfcc5d
laxa88 <gashzack@hotmail.com>
2017-12-12 21:45:12 +0800

    mini update to constants.js

--- src/constants.js
+++ src/constants.js
@@ -13,16 +13,16 @@ export const SPR_BUTTON = 'button';
 export const SPR_SHEET = 'icons';
 
 export const ICON_DATA = [
-  {name: 'behance.png', x: 32, y: 32, width: 32, height: 32, value: 1},
-  {name: 'dribbble.png', x: 64, y: 64, width: 32, height: 32, value: 1},
-  {name: 'facebook.png', x: 64, y: 32, width: 32, height: 32, value: 1},
-  {name: 'google.png', x: 64, y: 0, width: 32, height: 32, value: 1},
-  {name: 'like.png', x: 32, y: 96, width: 32, height: 32, value: 1},
-  {name: 'linkedin.png', x: 32, y: 64, width: 32, height: 32, value: 1},
-  {name: 'pinterest.png', x: 64, y: 96, width: 32, height: 32, value: 1},
-  {name: 'rss.png', x: 32, y: 0, width: 32, height: 32, value: 1},
-  {name: 'stumbleupon.png', x: 0, y: 96, width: 32, height: 32, value: 1},
-  {name: 'twitter.png', x: 0, y: 64, width: 32, height: 32, value: 1},
-  {name: 'vimeo.png', x: 0, y: 32, width: 32, height: 32, value: 1},
-  {name: 'youtube.png', x: 0, y: 0, width: 32, height: 32, value: 1},
+  {key: 'behance.png', x: 32, y: 32, width: 32, height: 32, value: 1},
+  {key: 'dribbble.png', x: 64, y: 64, width: 32, height: 32, value: 2},
+  {key: 'facebook.png', x: 64, y: 32, width: 32, height: 32, value: 3},
+  {key: 'google.png', x: 64, y: 0, width: 32, height: 32, value: 4},
+  {key: 'like.png', x: 32, y: 96, width: 32, height: 32, value: 5},
+  {key: 'linkedin.png', x: 32, y: 64, width: 32, height: 32, value: 6},
+  {key: 'pinterest.png', x: 64, y: 96, width: 32, height: 32, value: 7},
+  {key: 'rss.png', x: 32, y: 0, width: 32, height: 32, value: 8},
+  {key: 'stumbleupon.png', x: 0, y: 96, width: 32, height: 32, value: 9},
+  {key: 'twitter.png', x: 0, y: 64, width: 32, height: 32, value: 10},
+  {key: 'vimeo.png', x: 0, y: 32, width: 32, height: 32, value: 11},
+  {key: 'youtube.png', x: 0, y: 0, width: 32, height: 32, value: 12},
 ];
