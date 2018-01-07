497b95a5bed11b21e8b7d8af049207b11927dcd7
laxa88 <gashzack@hotmail.com>
2018-01-07 14:59:01 +0800

    Split webpack config into dev and release

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,7 @@
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
 module.exports = {
   devtool: 'source-map',
   entry: './app.js',
   output: {
     filename: './build/bundle.js',
   },
-  plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true,
-    }),
-  ],
 };
